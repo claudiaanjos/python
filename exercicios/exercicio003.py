bae1e8773b0a89145d62a4a569892e821f57d9e8
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-09-30 11:31:03 -0300

    exercicio003

---
+++ exercicios/exercicio003.py
@@ -0,0 +1,8 @@
+"""
+Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+"""
+
+n1 = input('Primeiro número ')
+n2 = input('Segundo número ')
+
+print('A soma é', n1 + n2)
