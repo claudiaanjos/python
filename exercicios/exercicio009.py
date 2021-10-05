e84352069ad02e38e0914d3bb1cbe3ab53c715db
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2021-10-05 15:31:17 -0300

    exercicio009

---
+++ exercicios/exercicio009.py
@@ -0,0 +1,16 @@
+"""
+Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+"""
+
+n = int(input('\033[4;34mEscreva um número inteiro para saber sua tabuada:\033[m '))
+
+print(f'\033[0;31m{n} x 1 = {n} ')
+print(f'\033[0;31m{n} x 2 = {n*2} ')
+print(f'\033[0;31m{n} x 3 = {n*3} ')
+print(f'\033[0;31m{n} x 4 = {n*4} ')
+print(f'\033[0;33m{n} x 5 = {n*5} ')
+print(f'\033[0;33m{n} x 6 = {n*6} ')
+print(f'\033[0;32m{n} x 7 = {n*7} ')
+print(f'\033[0;32m{n} x 8 = {n*8}')
+print(f'\033[0;32m{n} x 9 = {n*9}')
+print(f'\033[0;32m{n} x 10 = {n*10}')
